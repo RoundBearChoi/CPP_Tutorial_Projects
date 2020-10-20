4a19923947f6d2a139ae990f8bc5d39e6ca939cc
RoundBearChoi <roundbeargames@gmail.com>
2020-10-20 20:01:13 +0900

    video #7

---
+++ Projects/007 grids and nested loops/007 grids and nested loops/007 grids and nested loops.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+struct GridObject
+{
+	int x;
+	int y;
+};
+
+struct Grid
+{
+	int totalX;
+	int totalY;
+
+	void Render(GridObject obj)
+	{
+		for (int col = 0; col < totalY; col++)
+		{
+			for (int row = 0; row < totalX; row++)
+			{
+				if (obj.x == row && obj.y == col)
+				{
+					std::cout << "♣";
+				}
+				else
+				{
+					std::cout << "□";
+				}
+			}
+
+			std::cout << std::endl;
+		}
+	}
+};
+
+int main()
+{
+	Grid grid{ 8, 12 };
+	GridObject obj{ 3, 6 };
+
+	grid.Render(obj);
+}
