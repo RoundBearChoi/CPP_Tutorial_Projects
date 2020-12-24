9e490cc044f9ed6fd10f9beabbf7ee9ede5816a1
RoundBearChoi <roundbeargames@gmail.com>
2020-12-24 19:37:17 +0900

    drawing sprites (part2)

--- Projects/Drawing Sprites/Drawing Sprites/Game.h
+++ Projects/Drawing Sprites/Drawing Sprites/Game.h
@@ -1,18 +1,36 @@
-#define OLC_PGE_APPLICATION char[] "define this however you want to"
 #pragma once
+#define OLC_PGE_APPLICATION 0
 #include "olcPixelGameEngine.h"
+//#include <iostream>
 
-class Game : olc::PixelGameEngine
+class Game : public olc::PixelGameEngine
 {
+private:
+	olc::Sprite* spritePtr = nullptr;
+
 public:
+	~Game()
+	{
+		delete spritePtr;
+	}
+
 	bool OnUserCreate() override
 	{
+		//std::filesystem::path startingPath = std::filesystem::current_path();
+		spritePtr = new olc::Sprite("smiley.png");
 
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		Clear(olc::DARK_BLUE);
+
+		olc::vf2d position(10, 50);
+		DrawSprite(position, spritePtr);
+
+		olc::vf2d stringposition(0, 0);
+		DrawString(stringposition, "this is a string", olc::DARK_RED);
 
 		return true;
 	}
