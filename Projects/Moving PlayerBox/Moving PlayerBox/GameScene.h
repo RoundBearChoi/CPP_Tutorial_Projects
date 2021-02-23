11ac91c748e8fa1b81bef0150a9f998ce8c4d02a
Choi <roundbeargames@gmail.com>
2021-02-23 11:17:02 +0900

    ptr and render

--- Projects/Moving PlayerBox/Moving PlayerBox/GameScene.h
+++ Projects/Moving PlayerBox/Moving PlayerBox/GameScene.h
@@ -41,10 +41,15 @@ namespace RB
 
 		void RenderScene(olc::PixelGameEngine* ptrEngine) override
 		{
-			objTree.GetObjType(GameObjType::background)->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::background), RenderOffsetType::NONE);
-			objTree.GetObjType(GameObjType::player)->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::playerbox_green), RenderOffsetType::BOTTOM_CENTER);
-			objTree.GetObjType(GameObjType::player)->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::diamond_red), RenderOffsetType::CENTER_CENTER);
-			objTree.GetObjType(GameObjType::shitgroup)->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::redbox_10x10), RenderOffsetType::CENTER_CENTER);
+			GameObj* background = objTree.GetObjType(GameObjType::background);
+			GameObj* playerBox = objTree.GetObjType(GameObjType::player);
+			GameObj* playerPoint = objTree.GetObjType(GameObjType::player);
+			GameObj* shitgroup = objTree.GetObjType(GameObjType::shitgroup);
+
+			background->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::background), RenderOffsetType::NONE);
+			playerBox->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::playerbox_green), RenderOffsetType::BOTTOM_CENTER);
+			playerPoint->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::diamond_red), RenderOffsetType::CENTER_CENTER);
+			shitgroup->Render(ptrEngine, decalLoader.GetDecal((int)SpriteType::redbox_10x10), RenderOffsetType::CENTER_CENTER);
 		}
 	};
 }
