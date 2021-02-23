7abdc784a6d27273da305826938d916ed02ea1fa
Choi <roundbeargames@gmail.com>
2021-02-23 14:47:13 +0900

    deleted cpp, cout creation id instead of type

--- Projects/Moving PlayerBox/Moving PlayerBox/GameObj.h
+++ Projects/Moving PlayerBox/Moving PlayerBox/GameObj.h
@@ -18,13 +18,82 @@ namespace RB
 	public:
 		ObjData data;
 
-		GameObj(ObjSpecs specs);
-		~GameObj();
+		GameObj(ObjSpecs specs)
+		{
+			std::cout << "construcing GameObj" << std::endl;
+			objType = specs.objType;
+		}
 
-		bool IsObjType(GameObjType _objType);
-		void Render(olc::PixelGameEngine* engine, olc::Decal* decal, RenderOffsetType renderOffset);
-		void AddToHierarchy(GameObj* child);
-		void SetController(ControllerType _controllerType);
-		ObjController* GetController();
+		~GameObj()
+		{
+			std::cout << "destructing GameObj" << std::endl;
+
+			if (ptrController != nullptr)
+			{
+				delete ptrController;
+			}
+		}
+
+		bool IsObjType(GameObjType _objType)
+		{
+			if (objType == _objType)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		void Render(olc::PixelGameEngine* engine, olc::Decal* decal, RenderOffsetType renderOffset)
+		{
+			olc::vf2d offset(0.0f, 0.0f);
+			float decalWidth = (float)(decal->sprite->width);
+			float decalHeight = (float)(decal->sprite->height);
+
+			if (renderOffset == RenderOffsetType::NONE)
+			{
+				// do nothing
+			}
+			else if (renderOffset == RenderOffsetType::CENTER_CENTER)
+			{
+				offset.x = -(decalWidth / 2.0f);
+				offset.y = -(decalHeight / 2.0f);
+			}
+			else if (renderOffset == RenderOffsetType::BOTTOM_CENTER)
+			{
+				offset.x = -(decalWidth / 2.0f);
+				offset.y = -(decalHeight);
+			}
+
+			engine->DrawDecal(data.position + offset, decal);
+		}
+
+		void AddToHierarchy(GameObj* child)
+		{
+			vecChildrenPtr.push_back(child);
+		}
+
+		void SetController(ControllerType _controllerType)
+		{
+			if (_controllerType == ControllerType::PLAYER_CONTROLLER)
+			{
+				ptrController = new PlayerController();
+			}
+			else if (_controllerType == ControllerType::SHIT_CONTROLLER_GENERATOR)
+			{
+				ptrController = new ShitGeneratorController();
+			}
+			else if (_controllerType == ControllerType::SHIT_CONTROLLER_FALL)
+			{
+				ptrController = new ShitFallController();
+			}
+		}
+
+		ObjController* GetController()
+		{
+			return ptrController;
+		}
 	};
 }
