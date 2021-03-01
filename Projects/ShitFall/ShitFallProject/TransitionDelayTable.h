908e519e921bb4520b28b0948b4864480c57148f
Choi <roundbeargames@gmail.com>
2021-03-01 17:49:12 +0900

    tune

--- Projects/ShitFall/ShitFallProject/TransitionDelayTable.h
+++ Projects/ShitFall/ShitFallProject/TransitionDelayTable.h
@@ -6,7 +6,7 @@ namespace RB
 	{
 	public:
 		static const int PLAYER_IDLE = 30;
-		static const int PLAYER_RUN = 6;
+		static const int PLAYER_RUN = 4;
 		static const int PLAYER_DEATH = 2;
 		static const int SHIT_UPSPLASH = 4;
 	};
