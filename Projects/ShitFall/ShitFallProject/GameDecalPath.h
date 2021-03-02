53223f15828a2d79fb56fdbbbba7928d8beda8ec
Choi <roundbeargames@gmail.com>
2021-03-03 03:00:08 +0900

    switch

--- Projects/ShitFall/ShitFallProject/GameDecalPath.h
+++ Projects/ShitFall/ShitFallProject/GameDecalPath.h
@@ -16,53 +16,20 @@ namespace RB
 		{
 			GameSpriteType spriteType = (GameSpriteType)_index;
 
-			if (spriteType == GameSpriteType::playerbox_green)
+			switch (spriteType)
 			{
-				return "PNG files/playerbox_green.png";
-			}
-			else if (spriteType == GameSpriteType::background)
-			{
-				return "PNG files/background.png";
-			}
-			else if (spriteType == GameSpriteType::redbox_10x10)
-			{
-				return "PNG files/redbox_10x10.png";
-			}
-			else if (spriteType == GameSpriteType::shit_box32x32)
-			{
-				return "PNG files/shit_box32x32.png";
-			}
-			else if (spriteType == GameSpriteType::pixel_run_sheet)
-			{
-				return "PNG files/pixel_run_sheet.png";
-			}
-			else if (spriteType == GameSpriteType::idle_sheet)
-			{
-				return "PNG files/idle_sheet.png";
-			}
-			else if (spriteType == GameSpriteType::y_or_n460x330)
-			{
-				return "PNG files/y_or_n460x330.png";
-			}
-			else if (spriteType == GameSpriteType::shit_restart)
-			{
-				return "PNG files/shitrestart.png";
-			}
-			else if (spriteType == GameSpriteType::shit)
-			{
-				return "PNG files/shit.png";
-			}
-			else if (spriteType == GameSpriteType::ko_sheet_2)
-			{
-				return "PNG files/ko_sheet_2.png";
-			}
-			else if (spriteType == GameSpriteType::upsplash_sheet_3)
-			{
-				return "PNG files/upsplash_sheet_3.png";
-			}
-			else if (spriteType == GameSpriteType::centersplash_sheet)
-			{
-				return "PNG files/centersplash_sheet.png";
+			case GameSpriteType::playerbox_green: return "PNG files/playerbox_green.png";
+			case GameSpriteType::background: return "PNG files/background.png";
+			case GameSpriteType::redbox_10x10: return "PNG files/redbox_10x10.png";
+			case GameSpriteType::shit_box32x32: return "PNG files/shit_box32x32.png";
+			case GameSpriteType::pixel_run_sheet: return "PNG files/pixel_run_sheet.png";
+			case GameSpriteType::idle_sheet: return "PNG files/idle_sheet.png";
+			case GameSpriteType::y_or_n460x330: return "PNG files/y_or_n460x330.png";
+			case GameSpriteType::shit_restart: return "PNG files/shitrestart.png";
+			case GameSpriteType::shit: return "PNG files/shit.png";
+			case GameSpriteType::ko_sheet_2: return "PNG files/ko_sheet_2.png";
+			case GameSpriteType::upsplash_sheet_3: return "PNG files/upsplash_sheet_3.png";
+			case GameSpriteType::centersplash_sheet: return "PNG files/centersplash_sheet.png";
 			}
 
 			return "none";
